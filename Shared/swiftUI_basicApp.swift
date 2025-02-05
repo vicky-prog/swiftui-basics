8839c9797c841143c68677e0f27b68bef4ff9a25
vicky-prog <johnvick281997@gmail.com>
2025-02-06 00:03:11 +0530

    first commit

--- Shared/swiftUI_basicApp.swift
+++ Shared/swiftUI_basicApp.swift
@@ -5,13 +5,41 @@
 //  Created by vignesh on 05/02/25.
 //
 
+
+
+
 import SwiftUI
 
 @main
 struct swiftUI_basicApp: App {
+    
+    @Environment(\.scenePhase) var scenePhase
+    
+    init(){
+        print("App is initializing")
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear{
+                    print("ContentView appeared")
+                }
+                .onDisappear(){
+                    print("ContentView disappeared")
+                }
+        }
+        .onChange(of: scenePhase){ newPhase in
+            switch newPhase {
+                       case .active:
+                           print("App is active")
+                       case .inactive:
+                           print("App is inactive")
+                       case .background:
+                           print("App is in background")
+                       @unknown default:
+                           break
+                       }
+            
         }
     }
 }
