eed53ac253b6d87e749363460cb21e8a445b0cfa
vicky-prog <johnvick281997@gmail.com>
2025-02-05 23:31:14 +0530

    Initial Commit

---
+++ Shared/swiftUI_basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_basicApp.swift
+//  Shared
+//
+//  Created by vignesh on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
