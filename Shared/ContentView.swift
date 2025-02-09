fd09a839c71918101e38def7171d712e1f22746c
vignesh <johnvick281997@gmail.com>
2025-02-09 14:39:55 +0530

    pass data between screen

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,15 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var inputText: String = ""
     var body: some View {
+       NavigationView {
+        VStack{
         Text("Hello, world!")
             . shadow(radius: 0)
             .padding()
+            .foregroundColor(.blue)
+            
+            TextField("Type Something..", text: $inputText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                   .padding()
+           
+            NavigationLink(destination: DetailsView(inputValue: $inputText)) {
+                               Text("Go to Details")
+                           }
     }
+    }
+       .navigationTitle("Home")
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+}
+
+struct DetailsView: View {
+    @Binding var inputValue:String
+    @State var updatedText:String = "Hello"
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View {
+        VStack{
+            Text(inputValue)
+            Button("Go back and send value"){
+               inputValue = updatedText
+                presentationMode.wrappedValue.dismiss()
+            }
+            
+        }
+        
     }
 }
